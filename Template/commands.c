706e4f492713e7f5d8f3f78ba044984885a7d119
pitex <michalpiekarz09@gmail.com>
2013-12-17 23:24:31 +0100

    removed printfs

--- Template/commands.c
+++ Template/commands.c
@@ -17,124 +17,150 @@ int m_lenv(char*[]);
 int m_ls(char*[]);
 
 command_pair dispatch_table[]={
-    {"echo", &echo},
-    {"exit", &m_exit},
-    {"cd", &m_cd},
-    {"kill", &m_kill},
-    {"lenv", &m_lenv},
-    {"lls", &m_ls},
-    {NULL,NULL}
+	{"echo", &echo},
+	{"exit", &m_exit},
+	{"cd", &m_cd},
+	{"kill", &m_kill},
+	{"lenv", &m_lenv},
+	{"lls", &m_ls},
+	{NULL,NULL}
 };
 
 //	Checks if string can be converted to int
 int is_int(char *a)
 {
-    int i;
-    int length = strlen(a);
-    int beg = 0;
-    int result = 0;
-    beg+=(a[0]=='-');
-    
-    for (i=beg; i<length; i++)
-    {
-        if (a[i]>'9' || a[i]<'0') return 0;
-    }
-    
-    return 1;
+	int i;
+	int length = strlen(a);
+	int beg = 0;
+	int result = 0;
+	beg+=(a[0]=='-');
+	
+	for (i=beg; i<length; i++)
+	{
+		if (a[i]>'9' || a[i]<'0') return 0;
+	}
+	
+	return 1;
 }
 
 //	Converts string to int
 int to_int(char *a)
 {
-    int i;
-    int length = strlen(a);
-    int beg = 0;
-    int result = 0;
-    beg+=(a[0]=='-');
-    
-    for (i=beg; i<length; i++)
-    {
-        result*=10;
-        result+=a[i]-'0';
-    }
-    return beg?-result:result;
+	int i;
+	int length = strlen(a);
+	int beg = 0;
+	int result = 0;
+	beg+=(a[0]=='-');
+	
+	for (i=beg; i<length; i++)
+	{
+		result*=10;
+		result+=a[i]-'0';
+	}
+	return beg?-result:result;
+}
+
+void write_string(char * string) {
+	int temp;
+	int printed = 0;
+	int size = strlen(string);
+	char * to_print = string;
+
+	while (printed<size) {
+		temp = write(STDOUT_FILENO,to_print,size-printed);
+		if (temp == -1) {
+			exit(EXIT_FAILURE);
+		}
+		printed+=temp;
+	}
 }
 
 int echo(argv)
 char * argv[];
 {
-    int i =1;
-    if (argv[i]) printf("%s", argv[i++]);
-    while  (argv[i])
-        printf(" %s", argv[i++]);
-    printf("\n");
-    fflush(stdout);
-    return 0;
+	int i =1;
+	if (argv[i]) write_string(argv[i++]);//printf("%s", argv[i++]);
+	while  (argv[i]){
+		write_string(" ");
+		write_string(argv[i++]);
+		// printf(" %s", argv[i++]);
+	}
+	write_string("\n");
+	// printf("\n");
+	fflush(stdout);
+	return 0;
 }
 
 int m_exit(argv)
 char * argv[];
 {
-    int i;
-    int id = 0;
+	int i;
+	int id = 0;
 
-    if (argv[1])
-    {
-        if (!is_int(argv[1]))
-        {
-            printf("mshell: exit: %s: numeric argument required\n",argv[0]);
-            exit(255);
-        }
-        
-        id = to_int(argv[1]);
-        if (id<0)
-        {
-            printf("mshell: exit: Illegal number: %d\n",id);
-            return -1;
-        }
-        exit(id);
-    }
-    else
-    {
-        exit(EXIT_SUCCESS);
-    }
-    
-    return -1;
+	if (argv[1])
+	{
+		if (!is_int(argv[1]))
+		{
+			write_string("mshell: exit: ");
+			write_string(argv[0]);
+			write_string(": numeric argument required\n");
+			// printf("mshell: exit: %s: numeric argument required\n",argv[0]);
+			exit(255);
+		}
+		
+		id = to_int(argv[1]);
+		if (id<0)
+		{
+			write_string("mshell: exit: Illegal number: ");
+			write_string(argv[1]);
+			write_string("\n");
+			// printf("mshell: exit: Illegal number: %d\n",id);
+			return -1;
+		}
+		exit(id);
+	}
+	else
+	{
+		exit(EXIT_SUCCESS);
+	}
+	
+	return -1;
 }
 
 int m_cd(argv)
 char * argv[];
 {
-    if (argv[1])
-    {
-        chdir(argv[1]);
-    }
-    else
-    {
-        chdir(getenv("HOME"));
-    }
-    return 0;
+	if (argv[1])
+	{
+		chdir(argv[1]);
+	}
+	else
+	{
+		chdir(getenv("HOME"));
+	}
+	return 0;
 }
 
 int m_kill(argv)
 char * argv[];
 {
-    if (!argv[1])
-    {
-        printf("kill: usage: kill [-signal] pid");
-        return -1;
-    }
-    if (!argv[1])
-    {
-        kill(to_int(argv[1]),0);
-    }
-    else
-    {
-        kill(to_int(argv[2]),to_int(argv[1]));
-    }
-    fflush(stdout);
-    
-    return 0;
+	if (!argv[1])
+	{
+		write_string("kill: usage: kill [-signal] pid\n");
+		// printf("kill: usage: kill [-signal] pid");
+		return -1;
+	}
+	if (!argv[2])
+	{
+		kill(to_int(argv[1]),0);
+	}
+	else
+	{
+		kill(to_int(argv[2]),to_int(argv[1]));
+	}
+	fflush(stdout);
+	
+	return 0;
 }
 
 extern char **environ;
@@ -142,32 +168,36 @@ extern char **environ;
 int m_lenv(argv)
 char * argv[];
 {
-    char **current;
-    for(current = environ; *current; current++) {
-        printf("%s\n",*current);
-    }
-    fflush(stdout);
+	char **current;
+	for(current = environ; *current; current++) {
+		write_string(*current);
+		write_string("\n");
+		// printf("%s\n",*current);
+	}
+	fflush(stdout);
 }
 
 int m_ls(argv)
 char * argv[];
 {
-    char dir_name[1024];
-    struct dirent *file;
+	char dir_name[1024];
+	struct dirent *file;
 
-    getcwd(dir_name,1024);
+	getcwd(dir_name,1024);
 
-    DIR* cur_dir = opendir(dir_name);
-    if (cur_dir=opendir(dir_name))
-    {
-        while (file = readdir(cur_dir))
-        {
-            if (file->d_name[0]!='.')
-            {
-                printf("%s\n",file->d_name);
-            }
-        }
-        closedir(cur_dir);
-    }
-    fflush(stdout);
+	DIR* cur_dir = opendir(dir_name);
+	if (cur_dir=opendir(dir_name))
+	{
+		while (file = readdir(cur_dir))
+		{
+			if (file->d_name[0]!='.')
+			{
+				write_string(file->d_name);
+				write_string("\n");
+				// printf("%s\n",file->d_name);
+			}
+		}
+		closedir(cur_dir);
+	}
+	fflush(stdout);
 }
--- Template/mshell.c
+++ Template/mshell.c
@@ -27,223 +27,223 @@ int children_pids[MAX_COMMANDS+10];
 
 //  Checks if input is special character input
 void check_input_type() {
-    struct stat file_stat;
-    fstat(0,&file_stat);
-    
-    if (S_ISCHR(file_stat.st_mode)) {
-        print_prompt = true;
-    }
+	struct stat file_stat;
+	fstat(0,&file_stat);
+	
+	if (S_ISCHR(file_stat.st_mode)) {
+		print_prompt = true;
+	}
 }
 
 //  Displays prompt
 int display_prompt() {
-    if (print_prompt) {
-        return write(1,PROMPT,PROMPT_LENGTH);
-    }
-    else {
-        return true;
-    }
+	if (print_prompt) {
+		return write(1,PROMPT,PROMPT_LENGTH);
+	}
+	else {
+		return true;
+	}
 }
 
 //  Reads input
 int read_input() {
-    int i;
+	int i;
 
-    if (buf_end > BUFF_SIZE) {
-        for (i = buf_begin; i<buf_end; i++) {
-            input_buffer[i-buf_begin] = input_buffer[i];
-        }
-        
-        buf_end -= buf_begin;
-        buf_begin = 0;
-    }
-    
-    return read(0,input_buffer + buf_end,BUFF_SIZE);
+	if (buf_end > BUFF_SIZE) {
+		for (i = buf_begin; i<buf_end; i++) {
+			input_buffer[i-buf_begin] = input_buffer[i];
+		}
+		
+		buf_end -= buf_begin;
+		buf_begin = 0;
+	}
+	
+	return read(0,input_buffer + buf_end,BUFF_SIZE);
 }
 
 //  Compares two strings
 int equals(char * a, char * b) {
-    int i;
-    int size_a,size_b;
-    size_a = strlen(a);size_b = strlen(b);
-    if (size_a!=size_b) return false;
-    
-    for (i=0; i<size_a; i++) {
-        if (a[i]!=b[i]) return false;
-    }
-    
-    return true;
+	int i;
+	int size_a,size_b;
+	size_a = strlen(a);size_b = strlen(b);
+	if (size_a!=size_b) return false;
+	
+	for (i=0; i<size_a; i++) {
+		if (a[i]!=b[i]) return false;
+	}
+	
+	return true;
 }
 
 //  Replace current file descriptor on STDIN_FILENO with path file
 void change_input(char * path) {
-    close(STDIN_FILENO);
-    
-    open(path,O_RDONLY);
+	close(STDIN_FILENO);
+	
+	open(path,O_RDONLY);
 }
 
 //  Replace current file descriptor on STDOUT_FILENO with path file
 void change_output(char * path, int append) {
-    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 
-    close(STDOUT_FILENO);
-    
-    if (append) {
-        open(path, O_RDWR | O_CREAT | O_APPEND, mode);
-    } else {
-        open(path, O_RDWR | O_CREAT | O_TRUNC, mode);
-    }
+	close(STDOUT_FILENO);
+	
+	if (append) {
+		open(path, O_RDWR | O_CREAT | O_APPEND, mode);
+	} else {
+		open(path, O_RDWR | O_CREAT | O_TRUNC, mode);
+	}
 }
 
 //  Check if command is shell command
 int shell_command(command_s command) {
-    int i=0;
-    int stdout_copy;
+	int i=0;
+	int stdout_copy;
 
-    //  Go through dispatch table
-    while (i=0; dispatch_table[i].name != NULL; i++) {
-        if (equals(dispatch_table[i].name,command.argv[0])) {
-            //  If lenv has redirected output, do it
-            if (equals(command.argv[0],"lenv") && command.out_file_name) {
-                stdout_copy = dup(STDOUT_FILENO);
-                change_output(command.out_file_name,command.append_mode);
-            }
+	//  Go through dispatch table
+	for (i=0; dispatch_table[i].name != NULL; i++) {
+		if (equals(dispatch_table[i].name,command.argv[0])) {
+			//  If lenv has redirected output, do it
+			if (equals(command.argv[0],"lenv") && command.out_file_name) {
+				stdout_copy = dup(STDOUT_FILENO);
+				change_output(command.out_file_name,command.append_mode);
+			}
 
-            dispatch_table[i].fun(command.argv);
+			dispatch_table[i].fun(command.argv);
 
-            //  Clean up after redirecting output
-            if (equals(command.argv[0],"lenv") && command.out_file_name) {
-                close(STDOUT_FILENO);
-                dup2(stdout_copy,STDOUT_FILENO);
-                close(stdout_copy);
-            }
+			//  Clean up after redirecting output
+			if (equals(command.argv[0],"lenv") && command.out_file_name) {
+				close(STDOUT_FILENO);
+				dup2(stdout_copy,STDOUT_FILENO);
+				close(stdout_copy);
+			}
 
-            return true;
-        }
-    }
-    
-    return false;
+			return true;
+		}
+	}
+	
+	return false;
 }
 
 void execute_line() {
-    int i=0;
-    int j;
-    int pipe_fd[2];
-    int write_to = -1;
-    int read_from = -1;
-    int pid;
-    command_s* commands = split_commands(input_buffer+buf_begin);
+	int i=0;
+	int j;
+	int pipe_fd[2];
+	int write_to = -1;
+	int read_from = -1;
+	int pid;
+	command_s* commands = split_commands(input_buffer+buf_begin);
 
-    //  If there are no commands
-    if (commands[0].argv == NULL) {
-        return;
-    }
-    
-    //  If it is shell command
-    if (shell_command(commands[0])) {
-        return;
-    }
-    
-    //  Go through commands
-    for(i=0; commands[i].argv != NULL; i++) {
-        //  If there is a pipe after command, get ready to redirect output
-        if (commands[i+1].argv)
-        {
-            pipe(pipe_fd);
-            write_to = pipe_fd[WRITE_END];
-        }
-        
-        if (!(pid = fork())) {
-            //  Redirect output if needed
-            if (write_to != -1) {
-                dup2(write_to,STDOUT_FILENO);
-                close(write_to);
-                close(pipe_fd[READ_END]);
-            }
-            //  Redirect input if needed
-            if (read_from != -1) {
-                dup2(read_from,STDIN_FILENO);
-                close(read_from);
-                close(pipe_fd[WRITE_END]);
-            }
-            
-            //  Redirect input if "<"
-            if (commands[i].in_file_name) {
-                change_input(commands[i].in_file_name);
-            }
-            
-            //  Redirect output if ">" or ">>"
-            if (commands[i].out_file_name) {
-                change_output(commands[i].out_file_name, commands[i].append_mode);
-            }
-            
-            //  Execute command
-            if (execvp(commands[i].argv[0], commands[i].argv) == fail) {
-                exit(EXIT_FAILURE);
-            }
-        } else if (pid > 0) {
-            //  Close pipe
-            if (write_to != -1) close(write_to);
-            if (read_from != -1) close(read_from);
-            
-            //  Remember child pid
-            children_pids[i] = pid;
-        }
-        
-        //  Get ready to redirect input of the next command
-        read_from = pipe_fd[READ_END];
-        write_to = -1;
-    }
+	//  If there are no commands
+	if (commands[0].argv == NULL) {
+		return;
+	}
+	
+	//  If it is shell command
+	if (shell_command(commands[0])) {
+		return;
+	}
+	
+	//  Go through commands
+	for(i=0; commands[i].argv != NULL; i++) {
+		//  If there is a pipe after command, get ready to redirect output
+		if (commands[i+1].argv)
+		{
+			pipe(pipe_fd);
+			write_to = pipe_fd[WRITE_END];
+		}
+		
+		if (!(pid = fork())) {
+			//  Redirect output if needed
+			if (write_to != -1) {
+				dup2(write_to,STDOUT_FILENO);
+				close(write_to);
+				close(pipe_fd[READ_END]);
+			}
+			//  Redirect input if needed
+			if (read_from != -1) {
+				dup2(read_from,STDIN_FILENO);
+				close(read_from);
+				close(pipe_fd[WRITE_END]);
+			}
+			
+			//  Redirect input if "<"
+			if (commands[i].in_file_name) {
+				change_input(commands[i].in_file_name);
+			}
+			
+			//  Redirect output if ">" or ">>"
+			if (commands[i].out_file_name) {
+				change_output(commands[i].out_file_name, commands[i].append_mode);
+			}
+			
+			//  Execute command
+			if (execvp(commands[i].argv[0], commands[i].argv) == fail) {
+				exit(EXIT_FAILURE);
+			}
+		} else if (pid > 0) {
+			//  Close pipe
+			if (write_to != -1) close(write_to);
+			if (read_from != -1) close(read_from);
+			
+			//  Remember child pid
+			children_pids[i] = pid;
+		}
+		
+		//  Get ready to redirect input of the next command
+		read_from = pipe_fd[READ_END];
+		write_to = -1;
+	}
 
-    //  Wait for children to finish
-    for (j=0; j<i; j++) {
-        waitpid(children_pids[j],NULL,0);
-    }
+	//  Wait for children to finish
+	for (j=0; j<i; j++) {
+		waitpid(children_pids[j],NULL,0);
+	}
 }
 
 //  Decide what to do with read input
 void handle_input(int l) {
-    int i;
-    for (i=0; i<l; i++) {
-        //  If there is a whole line in buffer, execute it
-        if (input_buffer[buf_end + i] == '\n') {
-            input_buffer[buf_end + i] = '\0';
-            
-            execute_line();
-            
-            buf_begin = buf_end+i+1;
-        }
-    }
-    
-    buf_end += l;
+	int i;
+	for (i=0; i<l; i++) {
+		//  If there is a whole line in buffer, execute it
+		if (input_buffer[buf_end + i] == '\n') {
+			input_buffer[buf_end + i] = '\0';
+			
+			execute_line();
+			
+			buf_begin = buf_end+i+1;
+		}
+	}
+	
+	buf_end += l;
 }
 
 int main(argc, argv)
 int argc;
 char* argv[];
 {
-    int input_length;
-    
-    check_input_type();
+	int input_length;
+	
+	check_input_type();
 
-    while (true) {
-        
-        if (display_prompt() == fail) {
-            continue;
-        }
-        //  If there is nothing more to read, finish
-        if ((input_length = read_input()) == 0) {
-            break;
-        }
-        //  If you can't read due to interruption, try again! elsewise just give up
-        if (input_length < 0) {
-            if (errno == EINTR) continue;
-            exit(EXIT_FAILURE);
-        }
-        //  What to do now?
-        handle_input(input_length);
-    }
-    
-    return 0;
+	while (true) {
+		
+		if (display_prompt() == fail) {
+			continue;
+		}
+		//  If there is nothing more to read, finish
+		if ((input_length = read_input()) == 0) {
+			break;
+		}
+		//  If you can't read due to interruption, try again! elsewise just give up
+		if (input_length < 0) {
+			if (errno == EINTR) continue;
+			exit(EXIT_FAILURE);
+		}
+		//  What to do now?
+		handle_input(input_length);
+	}
+	
+	return 0;
 }
 
